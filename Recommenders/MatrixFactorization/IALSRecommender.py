469a6ce530f4283934a9d5a3176555fbe498dfaf
Benedart <49732385+Benedart@users.noreply.github.com>
2022-12-11 22:40:22 +0100

    :(

--- Recommenders/MatrixFactorization/IALSRecommender.py
+++ Recommenders/MatrixFactorization/IALSRecommender.py
@@ -236,7 +236,7 @@ class FeatureCombinedImplicitALSRecommender(BaseMatrixFactorizationRecommender):
             regularization=0.01,
             use_native=True, use_cg=True, use_gpu=False,
             calculate_training_loss=False, num_threads=0,
-            confidence_scaling=None,
+            confidence_scaling=linear_scaling_confidence,
             ):
 
         self.rec = implicit.als.AlternatingLeastSquares(factors=factors, regularization=regularization,
