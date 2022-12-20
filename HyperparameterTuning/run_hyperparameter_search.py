970ac8c44b12bd75a3d325eef1e11c5fa74a4898
Benedart <49732385+Benedart@users.noreply.github.com>
2022-12-20 22:09:40 +0100

    Update run_hyperparameter_search.py
    Re-added similarity types for ItemKNN_CFCBF

--- HyperparameterTuning/run_hyperparameter_search.py
+++ HyperparameterTuning/run_hyperparameter_search.py
@@ -265,13 +265,13 @@ def runHyperparameterSearch_Hybrid(recommender_class, URM_train, ICM_object, ICM
         if recommender_class in [ItemKNN_CFCBF_Hybrid_Recommender, UserKNN_CFCBF_Hybrid_Recommender]:
 
             if similarity_type_list is None:
-                similarity_type_list = ["tversky"]
+                similarity_type_list = ['cosine', 'jaccard', "asymmetric", "dice", "tversky"]
 
 
             hyperparameters_range_dictionary = {}
 
             if recommender_class is ItemKNN_CFCBF_Hybrid_Recommender:
-                hyperparameters_range_dictionary["ICM_weight"] = Real(low = 1e-2, high = 10, prior = 'log-uniform')
+                hyperparameters_range_dictionary["ICM_weight"] = Real(low = 1e-2, high = 1e2, prior = 'log-uniform')
 
             elif recommender_class is UserKNN_CFCBF_Hybrid_Recommender:
                 hyperparameters_range_dictionary["UCM_weight"] = Real(low = 1e-2, high = 1e2, prior = 'log-uniform')
